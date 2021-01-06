f25c41becae3c42d20b91d8c89e753d5bfc8a5ed
Oliver Kaufmann <kaufmann@novu.ch>
2021-01-06 03:27:59 +0100

    add files

---
+++ thesis2020/Playground2.cs
@@ -0,0 +1,26 @@
+namespace MiscDemos
+{
+
+    public class NameParser
+    {
+        public string GetLastName(string fullName)
+        {
+            var names = fullName.Split(" ");
+            var lastName = names.LastOrDefault();
+            return lastName ?? string.Empty;
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class NameParserBenchmarks
+    {
+        private const string FullName = "Steve J Gordon";
+        private static readonly NameParser Parser = new NameParser();
+
+        [Benchmark(Baseline = true)]
+        public void GetLastName()
+        {
+            Parser.GetLastName(FullName);
+        }
+    }
+}
---
+++ thesis2020/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var message = "Test 123123123";
+            var span = message.AsSpan();
+
+            Span<byte> bytes = stackalloc byte[2]; // Using C# 7.2 stackalloc support for spans
+            bytes[0] = 42;
+            bytes[1] = 43;
+            // Assert.Equal(42, bytes[0]);
+            // Assert.Equal(43, bytes[1]);
+            bytes[2] = 44; // throws IndexOutOfRangeException
+        }
+    }
+}
