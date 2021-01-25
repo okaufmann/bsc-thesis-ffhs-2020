3399fbf569f17b3f1597185c0650831c55a0df3b
Oliver Kaufmann <okaufmann91@gmail.com>
2021-01-25 19:09:38 +0100

    fix install script

--- scripts/install-env.sh
+++ scripts/install-env.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # Update system
-sudo apt-get update
-sudo apt-get upgrade
+apt update
+apt upgrade
 
 # Add .net source
 wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
 
 # install .net core SDK
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-3.1
+apt update
+apt install -y apt-transport-https
+apt update
+apt install -y dotnet-sdk-3.1
 
 # install Benchmark DotNet
-sudo dotnet tool install -g BenchmarkDotNet.Tool
+dotnet tool install -g BenchmarkDotNet.Tool
