3d86ef87964b83da0d9698e8e5e56ee7002e8729
Oliver Kaufmann <okaufmann91@gmail.com>
2021-01-25 21:32:32 +0100

    wip

--- scripts/run-benchmarks.sh
+++ scripts/run-benchmarks.sh
@@ -7,12 +7,17 @@ cd "$(dirname "$0")/.."
 cd src
 
 # cleanup
+echo "Cleaning up previous data"
 rm -Rf bin
 rm -Rf obj
 rm -Rf BenchmarkDotNet.Artifacts
 
 # run benchmarks
 # https://benchmarkdotnet.org/articles/guides/tool.html#help
+echo "Running benchmarks"
 dotnet run -c Release -- --runtimes netcoreapp31 --filter Thesis2020.Benchmarks*
 
-zip -r "results".zip "$1"
+echo "Packaging results"
+zip -r BenchmarkDotNet.Artifacts.zip BenchmarkDotNet.Artifacts
+mkdir -p results/
+mv BenchmarkDotNet.Artifacts.zip results/
