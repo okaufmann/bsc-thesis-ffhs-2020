9b4984a0f1e31390d040de713cd831d711ff5ca7
Oliver Kaufmann <okaufmann91@gmail.com>
2021-01-25 21:20:34 +0100

    add benchmark script

---
+++ scripts/run-benchmarks.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# ensure entrypoint is root directory
+cd "$(dirname "$0")/.."
+
+# enter benchmarks folder
+cd benchmarks
+
+# cleanup
+rm -Rf bin
+rm -Rf obj
+rm -Rf BenchmarkDotNet.Artifacts
+
+# run benchmarks
+# https://benchmarkdotnet.org/articles/guides/tool.html#help
+dotnet run -c Release -- --runtimes netcoreapp31 --filter Thesis2020.Benchmarks*
+
+zip -r "results".zip "$1"
