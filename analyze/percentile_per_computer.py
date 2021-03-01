d05e91759e892ea60a761b5a7abac5b402f7c165
Oliver Kaufmann <okaufmann91@gmail.com>
2021-03-01 23:49:22 +0100

    build percentile table

--- analyze/percentile_per_computer.py
+++ analyze/percentile_per_computer.py
@@ -1,36 +1,18 @@
 import pandas as pd
 import os.path as path
 import numpy as np
+from tabulate import tabulate
 
 cwd = path.dirname(__file__)
 
+
 # CPU Measurements
+outputFolder = "cpu"
 measurementColumn = 'Measurement_Value'
 
 # RAM Measurements
+# outputFolder = "ram"
 # measurementColumn = 'Allocated_Bytes'
-
-def percentiles(data, name):
-    minimum = np.percentile(data, 0)
-    lowerQuartile = np.percentile(data, 25)
-    median = np.percentile(data, 50)
-    higherQuartile = np.percentile(data, 75)
-    maximum = np.percentile(data, 100)
-    stdDev = np.std(data)
-    stdErr = np.std(data, ddof=1) / np.sqrt(np.size(data))
-    interquartileRange = higherQuartile - lowerQuartile
-
-    print(f"{name}, sample size: {len(data)})")
-    print("0th percentile: " + str(minimum))
-    print("25th percentile: " + str(lowerQuartile))
-    print("50th percentile (media): " + str(median))
-    print("75th percentile: " + str(higherQuartile))
-    print("100th percentile: " + str(maximum))
-    print("interquartile range: " + str(interquartileRange))
-    print("standard dev: " + str(stdDev))
-    print("standard err: " + str(stdErr))
-    print('-----------------------------------------')
-
 
 csvPath = path.join(cwd, '../results/all-data.csv')
 
@@ -55,12 +37,31 @@ for experiment, values in data:
 
     results[method][experiment[2]][experiment[3]][experiment[1]] = experimentValues
 
+df = pd.DataFrame(columns=['Name', '0th percentile', '25th percentile',
+                           '50th percentile (media)', '75th percentile', '100th percentile', 'interquartile range', 'standard deviation', 'standard error'])
 for method in results:
     for size in results[method]:
         for computer in results[method][size]:
             name = f"{computer}-{method} ({size})"
-            data = results[method][size][computer][dataType]
+            data = [results[method][size][computer][dataType]
+                    for dataType in results[method][size][computer]]
 
-            flatData = np.hstack(data)
+            data = np.hstack(data)
 
-            percentiles(flatData, name)
+            minPercentile = np.percentile(data, 0)
+            lowerQuartile = np.percentile(data, 25)
+            median = np.percentile(data, 50)
+            higherQuartile = np.percentile(data, 75)
+            maxPercentile = np.percentile(data, 100)
+            stdDev = np.std(data)
+            stdErr = np.std(data, ddof=1) / np.sqrt(np.size(data))
+            interquartileRange = higherQuartile - lowerQuartile
+
+            row = [name, minPercentile, lowerQuartile, median, higherQuartile, maxPercentile,interquartileRange, stdDev, stdErr]
+            append = {}
+            for i in range(len(df.columns)):
+                append[df.columns[i]] = row[i]
+            df = df.append(append, ignore_index=True)
+
+print(df.to_markdown())
+df.to_html(path.join(cwd, f'../results/statistics/{outputFolder}-percentile_per_computer.html'))
--- analyze/percentile_per_datatype.py
+++ analyze/percentile_per_datatype.py
@@ -1,38 +1,18 @@
 import pandas as pd
 import os.path as path
 import numpy as np
+from tabulate import tabulate
 
 cwd = path.dirname(__file__)
 
 
 # CPU Measurements
+# outputFolder = "cpu"
 # measurementColumn = 'Measurement_Value'
 
 # RAM Measurements
+outputFolder = "ram"
 measurementColumn = 'Allocated_Bytes'
-
-
-def percentiles(data, name):
-    minimum = np.percentile(data, 0)
-    lowerQuartile = np.percentile(data, 25)
-    median = np.percentile(data, 50)
-    higherQuartile = np.percentile(data, 75)
-    maximum = np.percentile(data, 100)
-    stdDev = np.std(data)
-    stdErr = np.std(data, ddof=1) / np.sqrt(np.size(data))
-    interquartileRange = higherQuartile - lowerQuartile
-
-    print(f"{name}, sample size: {len(data)})")
-    print("0th percentile: " + str(minimum))
-    print("25th percentile: " + str(lowerQuartile))
-    print("50th percentile (media): " + str(median))
-    print("75th percentile: " + str(higherQuartile))
-    print("100th percentile: " + str(maximum))
-    print("interquartile range: " + str(interquartileRange))
-    print("standard dev: " + str(stdDev))
-    print("standard err: " + str(stdErr))
-    print('-----------------------------------------')
-
 
 csvPath = path.join(cwd, '../results/all-data.csv')
 
@@ -46,7 +26,6 @@ for experiment, values in data:
     datatype = experiment[1]
     size = experiment[2]
 
-
     experimentValues = [
         value for value in values[measurementColumn]]
     if not method in results:
@@ -59,7 +38,8 @@ for experiment, values in data:
 
     # print(f"{method}-{datatype}-{size} {len(experimentValues)}")
 
-
+df = pd.DataFrame(columns=['Name', '0th percentile', '25th percentile',
+                           '50th percentile (media)', '75th percentile', '100th percentile', 'interquartile range', 'standard deviation', 'standard error'])
 for method in results:
     for datatype in results[method]:
         for size in results[method][datatype]:
@@ -67,4 +47,20 @@ for method in results:
 
             name = f"{method}-{datatype} ({size})"
 
-            percentiles(data, name)
+            minPercentile = np.percentile(data, 0)
+            lowerQuartile = np.percentile(data, 25)
+            median = np.percentile(data, 50)
+            higherQuartile = np.percentile(data, 75)
+            maxPercentile = np.percentile(data, 100)
+            stdDev = np.std(data)
+            stdErr = np.std(data, ddof=1) / np.sqrt(np.size(data))
+            interquartileRange = higherQuartile - lowerQuartile
+
+            row = [name, minPercentile, lowerQuartile, median, higherQuartile, maxPercentile,interquartileRange, stdDev, stdErr]
+            append = {}
+            for i in range(len(df.columns)):
+                append[df.columns[i]] = row[i]
+            df = df.append(append, ignore_index=True)
+
+print(df.to_markdown())
+df.to_html(path.join(cwd, f'../results/statistics/{outputFolder}-percentile_per_datatype.html'))
